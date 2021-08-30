f9d364cf7e18b36fb34066865252d1a128978e7d
jweaver <justinweaver1985@gmail.com>
2021-08-29 23:56:46 -0600

    checking on new code block coverage

--- src/lib/color_walk.py
+++ src/lib/color_walk.py
@@ -2,11 +2,11 @@ from .utils import translate, walk, randColor, render
 
 
 def new_untested_method(options):
-    options.new_thing = 'foo'
+    raise Exception()
 
 
 def color_walk(options):
-    if options.does_not_exist:
+    if len(options.buffer) > 100000:
         new_untested_method(options)
 
     # inital state if buffer has been cleared pick two random colors
