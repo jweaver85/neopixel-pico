9a616ca6f923c4bd2c740582c8e39d5612c1e486
jweaver <justinweaver1985@gmail.com>
2021-08-30 19:37:33 -0600

    adds tests

---
+++ tests/test_black_light.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from mock import patch
+
+from src.lib.black_light import black_light
+from .test_utils import create_options
+
+
+class TestBlackLight(TestCase):
+
+    @patch('src.lib.black_light.render')
+    def test_black_light(self, render):
+        options = create_options(num_pixels=5, step_size=255)
+        for i in range(options.num_pixels):
+            black_light(options)
+            self.assertEqual(len(options.buffer), options.num_pixels)
+            self.assertEqual(len(options.colors), i + 1)
