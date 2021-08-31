cffcdd4823ddf42da30d01fac2535f1e41e8696f
jweaver <justinweaver1985@gmail.com>
2021-08-30 20:13:56 -0600

    removest unreachable code

--- src/lib/color_walk.py
+++ src/lib/color_walk.py
@@ -1,13 +1,7 @@
 from .utils import translate, walk, randColor, render
 
 
-def new_untested_method(options):
-    raise Exception()
-
-
 def color_walk(options):
-    if len(options.buffer) > 100000:
-        new_untested_method(options)
 
     # inital state if buffer has been cleared pick two random colors
     if len(options.buffer) == 0:
